0709da57fb3141f29288aa5d7758d13472623cfc
selena <sln.wang@mail.utoronto.ca>
2022-10-07 16:52:23 -0400

    TODO's completed

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -179,7 +179,6 @@ public class Basics {
         if(arr.length != 1 & arr.length != 0){
             for(int i = 1; i < arr.length; i += 2){
                 current_sum += arr[i];
-
             }
         }
         return current_sum;
